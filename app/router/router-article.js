40912aae32527d2b99473e85054bb89169adb56f
felipemoura <felipe@icarustech.com.br>
2017-08-02 20:16:24 -0300

    Implements middleware

--- app/router/router-article.js
+++ app/router/router-article.js
@@ -1,10 +1,14 @@
 const express = require('express'),
   router = express.Router(),
-  ArticleController = require('../controllers/article');
+  ArticleController = require('../controllers/article'),
+  AuthMiddleware = require('../middlewares/auth-middleware');
+
 
 module.exports = function (app) {
   app.use('/article', router);
 };
+
+router.use(AuthMiddleware);
 
 router.get('/', ArticleController.index);
 router.post('/', ArticleController.newArticles);
